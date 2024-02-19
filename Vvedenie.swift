048a6822e44807b76e7985a28fc9e45b3aad5cef
Solferest <112902148+Solferest@users.noreply.github.com>
2024-02-19 13:27:07 +0300

    Create Vvedenie.swift

---
+++ Vvedenie.swift
@@ -0,0 +1,6 @@
+import Foundation
+let name: String = "Elizaveta Kuznetsova"
+let age: Int = 21
+let city: String = "Saint-Petersburg"
+let university: String = "Leti"
+print("Hello, my name is \(name). I am \(age) years old. I live in \(city) and study at \(university) university.")
